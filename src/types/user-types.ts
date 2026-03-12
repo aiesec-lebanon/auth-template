d985f7161e5145abb22169683f2e9b778562b829
Tharindu Wijekoon <wijekoontharindu80@gmail.com>
2026-03-12 15:49:10 +0530

    New types added

---
+++ src/types/user-types.ts
@@ -0,0 +1,22 @@
+export interface Role {
+  id: string;
+  name: string;
+}
+
+export interface Office {
+  id: string;
+  name: string;
+  tag: string;
+}
+
+export interface Position {
+  id: string;
+  office: Office;
+  role: Role;
+}
+
+export default interface UserInfo {
+  full_name: string;
+  profile_photo: string;
+  current_positions: Position[];
+}
